759d57ef11e7e6cd6185a1ec4d2c6779a62c0b7f
Mark Arsenault <admin@coindev.cc>
2014-05-10 17:19:25 -0400

    rel

--- src/base58.h
+++ src/base58.h
@@ -272,7 +272,7 @@ class CBitcoinAddress : public CBase58Data
 public:
     enum
     {
-        PUBKEY_ADDRESS = 38, // puzzlecoin addresses start with G
+        PUBKEY_ADDRESS = 55, // puzzlecoin addresses start with P
         SCRIPT_ADDRESS = 5,
         PUBKEY_ADDRESS_TEST = 111,
         SCRIPT_ADDRESS_TEST = 196,
